ea67263f1a17f6a8a268937bfc10a6fbdbbde431
Shchukin <a.a.shchukin@gmail.com>
2015-12-02 13:45:12 +0700

    Category word was removed from page title

--- functions.php
+++ functions.php
@@ -16,4 +16,26 @@ function nuqneH_widgets_init() {
 
 add_action( 'widgets_init', 'nuqneH_widgets_init' );
 
+
+
+/* 
+	Removing category title from page header
+	https://wordpress.org/support/topic/remove-the-word-category-before-title-of-category
+ */
+
+add_filter('gettext', 'remove_classifier_words', 20, 3);
+function remove_classifier_words( $translated_text, $untranslated_text, $domain ) {
+
+    $custom_field_text = 'Tag: %s';
+    if ( !is_admin() && $untranslated_text === $custom_field_text ) {
+        return '%s';
+    }
+
+    $custom_field_text = 'Category: %s';
+    if ( !is_admin() && $untranslated_text === $custom_field_text ) {
+        return '%s';
+    }
+
+    return $translated_text;
+}
 ?>
